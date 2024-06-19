65435169bd532cf1b17a584c5da0c9e738b4296d
aseplamri <aseplamri12@gmail.com>
2024-06-19 15:42:12 +0700

    Lagi

---
+++ Test Suites/Test Suite Login.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>55943bb8-c248-4bd7-8881-cf8fa0cde562</testSuiteGuid>
+</TestSuiteEntity>
